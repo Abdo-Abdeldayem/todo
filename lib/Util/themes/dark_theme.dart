7dfdc26d3d5be05c174cc2c92653eef55ce9131f
DESKTOP-2H9L9H5\H.DATA <abdelrhmanahed62@gmail.com>
2024-11-05 00:25:42 +0200

    themes_1

---
+++ lib/Util/themes/dark_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+ThemeData dark_Theme = ThemeData(
+  primarySwatch: Colors.teal,
+  colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: Colors.black,
+      onPrimary: Colors.white,
+      secondary: Colors.black45,
+      onSecondary: Colors.white30,
+      error: Colors.red,
+      onError: Colors.green,
+      surface: Colors.black54,
+      onSurface: Colors.white),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    backgroundColor: Colors.black,
+  ),
+);
